fcda4993ed8479b6c9e1b45bb3f697e450e8440b
HMJ07 <106169726+HMJ07@users.noreply.github.com>
2025-08-06 09:20:27 +0200

    Update generador_contraseñas.py
    ahora con caractéres especiales

--- generador_contraseñas.py
+++ generador_contraseñas.py
@@ -1,11 +1,15 @@
 import random
 import string
 print("\n------------------------Programa que genera contraseñas aleatorias--------------------------------\n")
-characters = string.ascii_letters+"1234567890"
+
+special_characters = "!@#$%^&*()_+-=[]{}|;:,.<>/?`~"
+
+characters = string.ascii_letters+string.digits+special_characters
 word=" "
 n = int (input("Introduce el número de caracteres: ") )
                                                   
 for i in range (n) :
-    random_letter = random. choice (characters)
+    random_letter = random.choice(characters)
     word = word + random_letter
 print (f"Tu contraseña generada automaticamente es: {word}")
+
